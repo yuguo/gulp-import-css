00a73a8c2d897acae7207e78ae0c8b41d4fe3d1e
yuguo <fishguo321@gmail.com>
2014-02-27 18:23:25 +0800

    change package

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ var gutil = require('gulp-util'),
   reworkImporter = require('rework-importer'),
   through = require('through2');
 
+// Consts
+const PLUGIN_NAME = 'gulp-import-css';
 
 module.exports = function() {
   
   return through.obj(function(file, enc, cb) {
     if (file.isStream()) {
-      this.emit('error', new gutil.PluginError('gulp-import-css', 'Streaming not supported'));
+      this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
       return cb();
     }
 
@@ -31,7 +33,7 @@ module.exports = function() {
         }))
         .toString();
     } catch(err) {
-      this.emit('error', new gutil.PluginError('gulp-import-css', err));
+      this.emit('error', new gutil.PluginError(PLUGIN_NAME, err));
       return cb();
     }
 

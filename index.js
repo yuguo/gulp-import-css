60ff8d8f0c18aec93c5e743fdb9acbf1d371ca73
Mickael Urrutia <mickael.urrutia@utc.fr>
2014-04-27 11:20:08 +0200

    force inline only for css files

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ module.exports = function() {
             return url;
           }
           var resourceAbsUrl = path.relative(file.base, path.resolve(path.dirname(file.path), url));
+          
+          // only css file are imported, we leave all other file imports (eot, woff, svg,...) as they are.
+          if (path.extname(resourceAbsUrl) != 'css') return url;
+                    
           return path.relative(destDir, resourceAbsUrl);
         }))
         .toString();
